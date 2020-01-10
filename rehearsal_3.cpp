b6bb89f45685ee285bf3bf3346c3cfb782d0bde6
xbunny <pitchayarat.p@gmail.com>
2020-01-10 15:52:35 +0700

    10012020

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,18 +1,28 @@
-#include <iostream>
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
 
-//Write sumSqrt() here.
-
+double sumSqrt(double N){
+	double i=1.00000, y=0.00000;
+	while(i<=N){
+		y+=(1.00000/sqrt(i));
+		i++;
+	}
+	return y;
+}
 
 int main()
 {
-    double a = sumSqrt(20);
-    double b = sumSqrt(10);
-    double c = sumSqrt(5);
-    double d = sumSqrt(2);
-    double e = sumSqrt(1);
-    double f = sumSqrt(0);
-    double g = sumSqrt(-1);
+	
+    double a = sumSqrt(20.00000);
+    double b = sumSqrt(10.00000);
+    double c = sumSqrt(5.00000);
+    double d = sumSqrt(2.00000);
+    double e = sumSqrt(1.00000);
+    double f = sumSqrt(0.00000);
+    double g = sumSqrt(-1.00000);
     
-    cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
-
+    cout <<fixed<<setprecision(5)<< a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
+	return 0;
 }
